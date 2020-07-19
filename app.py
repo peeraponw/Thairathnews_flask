9c8fc3c906c8e657b8d7855b96983a546b86a7b6
Peerapon Wechsuwanmanee <peerapon.w@gmail.com>
2020-07-19 12:22:14 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+#-*- coding=utf-8 -*-
+
+from datetime import datetime
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+import os
+import json
+from datetime import datetime
+import sys
+
+api = Flask(__name__)
+api.config['JSON_AS_ASCII'] = False
+
+
+URI = os.environ.get('MONGODB_URI')
+client = MongoClient(URI)
+db = client['heroku_95w86hmz']
+collection = db['thairath']
+
+@api.route('/api')
+def disp_news():
+    date = request.args.get('date', type=str)
+    tag = request.args.get('tag', type=str)
+    limit = request.args.get('limit', type=int, default=5)
+
+    if tag is not None:
+        query = {"tags": {"$elemMatch": {"$eq": tag} } }
+    if date is not None:
+        query = {"pub_date": date}
+    if (date is not None) and (tag is not None):
+        query = {"$and": [{"tags": {"$elemMatch": {"$eq": tag} } }, {"pub_date": date}] }
+
+    cursor = collection.find(query, projection={"_id":0}, limit=limit)
+    print(cursor.count())
+    results = []
+    for doc in cursor:
+        results.append(doc)
+    return jsonify(results)
+
+if __name__ == "__main__":
+    
+    api.run(debug=True)
